f7998f25afff83e45dd7feac2cf4f6d7ac46654d
validolsultan <158168231+amaraliss@users.noreply.github.com>
2025-03-01 15:33:22 +0300

    add Student

--- src/main/java/org/example/edu/controller/HelloController.java
+++ src/main/java/org/example/edu/controller/HelloController.java
@@ -1,5 +1,6 @@
 package org.example.edu.controller;
 
+import org.example.edu.model.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,10 @@ public class HelloController {
     @GetMapping ("/hello")
     public String hello(@RequestParam String name) {
         return "Hello" + name;
+    }
+    @GetMapping("/student")
+    public Student getStudent() {
+        Student st = new Student(1,"Valera","Valerik@ya.ru",19);
+        return st;
     }
 }
