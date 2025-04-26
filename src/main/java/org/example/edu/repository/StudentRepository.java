969ada4497d3f9220eb6ccc90648b03d4ae7427b
amaralis <158168231+amaraliss@users.noreply.github.com>
2025-04-26 10:47:57 +0300

    add repository

--- src/main/java/org/example/edu/repository/StudentRepository.java
+++ src/main/java/org/example/edu/repository/StudentRepository.java
@@ -10,3 +10,8 @@ import java.util.List;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 }
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    List<Student> findByName(String name);
+}
